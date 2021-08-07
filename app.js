5d06358c51509bc3bf60aa4795361b993c4151f3
Khaled Adad <khaledadad@lewisu.edu>
2021-08-07 12:54:04 -0500

    created and opened connection to DB

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize();
-
 const sequelize = new Sequelize({
     dialect: 'sqlite', // version of SQL you're using
     storage: 'movies.db' // specify the file path or the storage engine for SQLite
 });
 
 // Test the Connection
+(async() => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection to the database successful!');
+    } catch (error) {
+        console.error('Error connecting to the database: ', error);
+    }
+})();
