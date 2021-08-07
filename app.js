5e06c8f11c3be6ccfd0c1d41adb5db0823b6f20a
Khaled Adad <khaledadad@lewisu.edu>
2021-08-07 13:02:31 -0500

    added a Movies table

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const sequelize = new Sequelize({
     storage: 'movies.db' // specify the file path or the storage engine for SQLite
 });
 
+// Movie model
+class Movie extends Sequelize.Model {} // extending sequelize model to create Movie model subclass
+Movie.init({
+    title: Sequelize.STRING
+}, { sequelize }); // initialize, define a new table in the DB with the name Movie
+
+// sequelize: sequelize (above ^), can set a number of options in your model definition; for example, a custom name for your model and a timestamp. The only required option is a sequelize property that defines the sequelize instance to attach to the model
+
 // Test the Connection
 (async() => {
     try {
