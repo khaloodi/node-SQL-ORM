bd3453a8d7400b645dbd9530851f5d49620e3fc0
Khaled Adad <khaledadad@lewisu.edu>
2021-08-09 20:58:18 -0500

    added primary key column

--- db/models/movie.js
+++ db/models/movie.js
@@ -4,6 +4,15 @@ const Sequelize = require('sequelize');
 module.exports = (sequelize) => {
     class Movie extends Sequelize.Model {}
     Movie.init({
+        // Sequelize adds an id attribute to your model, which generates an 'id' column in your table that assigns each row a unique ID. The ID acts as a 'primary key', or a unique indexable reference for each entry.
+
+        // Instead of the default generated id column, you can set a custom column name for primary keys in your table, using primaryKey: true
+        // Set custom primary key column
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         title: {
             type: Sequelize.STRING
         },
